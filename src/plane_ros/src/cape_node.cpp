e7167e98298cd5ebf25c79f8690e6ddc759b6f5e
ranlong <s1899796@ed.ac.uk>
2021-03-26 12:11:50 +0000

    add all files into repo

---
+++ src/plane_ros/src/cape_node.cpp
@@ -0,0 +1,349 @@
+/*
+ * Copyright 2018 Pedro Proenza <p.proenca@surrey.ac.uk> (University of Surrey)
+ *
+ */
+
+#include <iostream>
+#include <cstdio>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <opencv2/opencv.hpp>
+#include <Eigen/Dense>
+#include "CAPE.h"
+
+#include <fstream>
+
+#include <ros/ros.h>
+
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/Image.h>
+#include <image_transport/subscriber_filter.h>
+
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+#include "tf/transform_datatypes.h"
+#include "tf_conversions/tf_eigen.h"
+//#include "tf/tf_eigen.h"
+#include "Eigen/Core"
+#include "Eigen/Geometry"
+#include <unsupported/Eigen/MatrixFunctions>
+
+
+using namespace std;
+
+bool loadCalibParameters(string filepath, cv:: Mat & intrinsics_rgb, cv::Mat & dist_coeffs_rgb, cv:: Mat & intrinsics_ir, cv::Mat & dist_coeffs_ir, cv::Mat & R, cv::Mat & T){
+
+    cv::FileStorage fs(filepath,cv::FileStorage::READ);
+    if (fs.isOpened()){
+        fs["RGB_intrinsic_params"]>>intrinsics_rgb;
+        fs["RGB_distortion_coefficients"]>>dist_coeffs_rgb;
+        fs["IR_intrinsic_params"]>>intrinsics_ir;
+        fs["IR_distortion_coefficients"]>>dist_coeffs_ir;
+        fs["Rotation"]>>R;
+        fs["Translation"]>>T;
+        fs.release();
+        return true;
+    }else{
+        cerr<<"Calibration file missing"<<endl;
+        return false;
+    }
+}
+
+void projectPointCloud(cv::Mat & X, cv::Mat & Y, cv::Mat & Z, cv::Mat & U, cv::Mat & V, float fx_rgb, float fy_rgb, float cx_rgb, float cy_rgb, double z_min, Eigen::MatrixXf & cloud_array){
+
+    int width = X.cols;
+    int height = X.rows;
+
+    // Project to image coordinates
+    cv::divide(X,Z,U,1);
+    cv::divide(Y,Z,V,1);
+    U = U*fx_rgb + cx_rgb;
+    V = V*fy_rgb + cy_rgb;
+    // Reusing U as cloud index
+    //U = V*width + U + 0.5;
+
+    float * sz, * sx, * sy, * u_ptr, * v_ptr, * id_ptr;
+    float z, u, v;
+    int id;
+    for(int r=0; r< height; r++){
+        sx = X.ptr<float>(r);
+        sy = Y.ptr<float>(r);
+        sz = Z.ptr<float>(r);
+        u_ptr = U.ptr<float>(r);
+        v_ptr = V.ptr<float>(r);
+        for(int c=0; c< width; c++){
+            z = sz[c];
+            u = u_ptr[c];
+            v = v_ptr[c];
+            if(z>z_min && u>0 && v>0 && u<width && v<height){
+                id = floor(v)*width + u;
+                cloud_array(id,0) = sx[c];
+                cloud_array(id,1) = sy[c];
+                cloud_array(id,2) = z;
+            }
+        }
+    }
+}
+
+void organizePointCloudByCell(Eigen::MatrixXf & cloud_in, Eigen::MatrixXf & cloud_out, cv::Mat & cell_map){
+
+    int width = cell_map.cols;
+    int height = cell_map.rows;
+    int mxn = width*height;
+    int mxn2 = 2*mxn;
+
+    int id, it(0);
+    int * cell_map_ptr;
+    for(int r=0; r< height; r++){
+        cell_map_ptr = cell_map.ptr<int>(r);
+        for(int c=0; c< width; c++){
+            id = cell_map_ptr[c];
+            *(cloud_out.data() + id) = *(cloud_in.data() + it);
+            *(cloud_out.data() + mxn + id) = *(cloud_in.data() + mxn + it);
+            *(cloud_out.data() + mxn2 + id) = *(cloud_in.data() + mxn2 + it);
+            it++;
+        }
+    }
+}
+
+
+class CapeParam {
+public:
+    stringstream calib_path;
+    int PATCH_SIZE;
+    cv::Mat K_rgb, K_ir, dist_coeffs_rgb, dist_coeffs_ir, R_stereo, t_stereo;
+
+    float fx_ir, fy_ir, cx_ir, cy_ir, fx_rgb, fy_rgb, cx_rgb, cy_rgb;
+
+    // cv::Mat rgb_img, d_img;
+    // int width, height;
+
+    // int nr_horizontal_cells = width/PATCH_SIZE;
+    // int nr_vertical_cells = height/PATCH_SIZE;
+
+    // // Pre-computations for backprojection
+    // cv::Mat_<float> X_pre(height,width);
+    // cv::Mat_<float> Y_pre(height,width);
+    // cv::Mat_<float> U(height,width);
+    // cv::Mat_<float> V(height,width);
+
+    // cv::Mat_<int> cell_map(height,width);
+
+    // cv::Mat_<float> X(height,width);
+    // cv::Mat_<float> Y(height,width);
+    // cv::Mat_<float> X_t(height,width);
+    // cv::Mat_<float> Y_t(height,width);
+    // Eigen::MatrixXf cloud_array(width*height,3);
+    // Eigen::MatrixXf cloud_array_organized(width*height,3);
+
+    CapeParam(int patch_size);
+
+    // void precompute(int w, int h);
+
+};
+
+CapeParam::CapeParam(int patch_size)
+{
+    PATCH_SIZE = patch_size;
+    calib_path<<"/home/ranlong/dev/plane_extraction/CAPE/Data/tunnel/calib_params.xml";
+    loadCalibParameters(calib_path.str(), K_rgb, dist_coeffs_rgb, K_ir, dist_coeffs_ir, R_stereo, t_stereo);
+    fx_ir = K_ir.at<double>(0,0);  fy_ir = K_ir.at<double>(1,1);
+    cx_ir = K_ir.at<double>(0,2);  cy_ir = K_ir.at<double>(1,2);
+    fx_rgb = K_rgb.at<double>(0,0);  fy_rgb = K_rgb.at<double>(1,1);
+    cx_rgb = K_rgb.at<double>(0,2);  cy_rgb = K_rgb.at<double>(1,2);
+}
+
+
+bool done = false;
+int im_count = 0;
+float COS_ANGLE_MAX = cos(M_PI/12);
+float MAX_MERGE_DIST = 50.0f;
+bool cylinder_detection= true;
+CAPE * plane_detector;
+CapeParam * param;
+std::vector<cv::Vec3b> color_code;
+
+int callback(const sensor_msgs::ImageConstPtr& msg_colour, const sensor_msgs::ImageConstPtr& msg_depth, CapeParam * param) {
+    cv::Mat rgb_img, d_img;
+    int width, height;
+
+    rgb_img = cv_bridge::toCvShare(msg_colour)->image;  // 8bit RGB image
+    d_img = cv_bridge::toCvShare(msg_depth)->image;   // 16bit depth image
+
+    if(rgb_img.size()!=d_img.size())
+        throw std::runtime_error("resolution mismatch");
+
+    if(rgb_img.data){
+        width = rgb_img.cols;
+        height = rgb_img.rows;
+    }else{
+        cout<<"Error loading file";
+        return -1;
+    }
+
+    if (im_count == 0)
+        plane_detector = new CAPE(height, width, param->PATCH_SIZE, param->PATCH_SIZE, cylinder_detection, COS_ANGLE_MAX, MAX_MERGE_DIST);
+        
+    int nr_horizontal_cells = width/param->PATCH_SIZE;
+    int nr_vertical_cells = height/param->PATCH_SIZE;
+
+    // Pre-computations for backprojection
+    cv::Mat_<float> X_pre(height,width);
+    cv::Mat_<float> Y_pre(height,width);
+    cv::Mat_<float> U(height,width);
+    cv::Mat_<float> V(height,width);
+    for (int r=0;r<height; r++){
+        for (int c=0;c<width; c++){
+            // Not efficient but at this stage doesn t matter
+            X_pre.at<float>(r,c) = (c-param->cx_ir)/param->fx_ir; Y_pre.at<float>(r,c) = (r-param->cy_ir)/param->fy_ir;
+        }
+    }
+
+    // Pre-computations for maping an image point cloud to a cache-friendly array where cell's local point clouds are contiguous
+    cv::Mat_<int> cell_map(height,width);
+
+    for (int r=0;r<height; r++){
+        int cell_r = r/param->PATCH_SIZE;
+        int local_r = r%param->PATCH_SIZE;
+        for (int c=0;c<width; c++){
+            int cell_c = c/param->PATCH_SIZE;
+            int local_c = c%param->PATCH_SIZE;
+            cell_map.at<int>(r,c) = (cell_r*nr_horizontal_cells+cell_c)*param->PATCH_SIZE*param->PATCH_SIZE + local_r*param->PATCH_SIZE + local_c;
+        }
+    }
+
+    cv::Mat_<float> X(height,width);
+    cv::Mat_<float> Y(height,width);
+    cv::Mat_<float> X_t(height,width);
+    cv::Mat_<float> Y_t(height,width);
+    Eigen::MatrixXf cloud_array(width*height,3);
+    Eigen::MatrixXf cloud_array_organized(width*height,3);
+
+    X = X_pre.mul(d_img); Y = Y_pre.mul(d_img);
+        cloud_array.setZero();
+
+    // The following transformation+projection is only necessary to visualize RGB with overlapped segments
+    // Transform point cloud to color reference frame
+    X_t = ((float)param->R_stereo.at<double>(0,0))*X+((float)param->R_stereo.at<double>(0,1))*Y+((float)param->R_stereo.at<double>(0,2))*d_img + (float)param->t_stereo.at<double>(0);
+    Y_t = ((float)param->R_stereo.at<double>(1,0))*X+((float)param->R_stereo.at<double>(1,1))*Y+((float)param->R_stereo.at<double>(1,2))*d_img + (float)param->t_stereo.at<double>(1);
+    d_img = ((float)param->R_stereo.at<double>(2,0))*X+((float)param->R_stereo.at<double>(2,1))*Y+((float)param->R_stereo.at<double>(2,2))*d_img + (float)param->t_stereo.at<double>(2);
+
+    projectPointCloud(X_t, Y_t, d_img, U, V, param->fx_rgb, param->fy_rgb, param->cx_rgb, param->cy_rgb, param->t_stereo.at<double>(2), cloud_array);
+
+    cv::Mat_<cv::Vec3b> seg_rz = cv::Mat_<cv::Vec3b>(height,width,cv::Vec3b(0,0,0));
+    cv::Mat_<uchar> seg_output = cv::Mat_<uchar>(height,width,uchar(0));
+
+    // Run CAPE
+    int nr_planes, nr_cylinders;
+    vector<PlaneSeg> plane_params;
+    vector<CylinderSeg> cylinder_params;
+    double t1 = cv::getTickCount();
+    organizePointCloudByCell(cloud_array, cloud_array_organized, cell_map);
+    plane_detector->process(cloud_array_organized, nr_planes, nr_cylinders, seg_output, plane_params, cylinder_params);
+    double t2 = cv::getTickCount();
+    double time_elapsed = (t2-t1)/(double)cv::getTickFrequency();
+    cout<<"Total time elapsed: "<<time_elapsed<<endl;
+
+    // Map segments with color codes and overlap segmented image w/ RGB
+    uchar * sCode;
+    uchar * dColor;
+    uchar * srgb;
+    int code;
+    for(int r=0; r<  height; r++){
+        dColor = seg_rz.ptr<uchar>(r);
+        sCode = seg_output.ptr<uchar>(r);
+        srgb = rgb_img.ptr<uchar>(r);
+        for(int c=0; c< width; c++){
+            code = *sCode;
+            if (code>0){
+                dColor[c*3] =   color_code[code-1][0]/2 + srgb[0]/2;
+                dColor[c*3+1] = color_code[code-1][1]/2 + srgb[1]/2;
+                dColor[c*3+2] = color_code[code-1][2]/2 + srgb[2]/2;;
+            }else{
+                dColor[c*3] =  srgb[0];
+                dColor[c*3+1] = srgb[1];
+                dColor[c*3+2] = srgb[2];
+            }
+            sCode++; srgb++; srgb++; srgb++;
+        }
+    }
+
+    // Show frame rate and labels
+    cv::rectangle(seg_rz,  cv::Point(0,0),cv::Point(width,20), cv::Scalar(0,0,0),-1);
+    std::stringstream fps;
+    fps<<(int)(1/time_elapsed+0.5)<<" fps";
+    cv::putText(seg_rz, fps.str(), cv::Point(15,15), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255,255,255,1));
+    cout<<"Nr cylinders:"<<nr_cylinders<<endl;
+    int cylinder_code_offset = 50;
+    // show cylinder labels
+    if (nr_cylinders>0){
+        std::stringstream text;
+        text<<"Cylinders:";
+        cv::putText(seg_rz, text.str(), cv::Point(width/2,15), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255,255,255,1));
+        for(int j=0;j<nr_cylinders;j++){
+            cv::rectangle(seg_rz,  cv::Point(width/2 + 80+15*j,6),cv::Point(width/2 + 90+15*j,16), cv::Scalar(color_code[cylinder_code_offset+j][0],color_code[cylinder_code_offset+j][1],color_code[cylinder_code_offset+j][2]),-1);
+        }
+    }
+    cv::imshow("Seg", seg_rz);
+    im_count++;
+
+
+}
+
+int main(int argc, char ** argv){
+    int PATCH_SIZE = 20;;
+    // if (argc>1){
+    //     PATCH_SIZE = atoi(argv[1]);
+    // }else{
+    //     PATCH_SIZE = 20;
+    // }
+    cv::namedWindow("Seg");    
+
+    // Populate with random color codes
+    for(int i=0; i<100;i++){
+        cv::Vec3b color;
+        color[0]=rand()%255;
+        color[1]=rand()%255;
+        color[2]=rand()%255;
+        color_code.push_back(color);
+    }
+
+    // Add specific colors for planes
+    color_code[0][0] = 0; color_code[0][1] = 0; color_code[0][2] = 255;
+    color_code[1][0] = 255; color_code[1][1] = 0; color_code[1][2] = 204;
+    color_code[2][0] = 255; color_code[2][1] = 100; color_code[2][2] = 0;
+    color_code[3][0] = 0; color_code[3][1] = 153; color_code[3][2] = 255;
+    // Add specific colors for cylinders
+    color_code[50][0] = 178; color_code[50][1] = 255; color_code[50][2] = 0;
+    color_code[51][0] = 255; color_code[51][1] = 0; color_code[51][2] = 51;
+    color_code[52][0] = 0; color_code[52][1] = 255; color_code[52][2] = 51;
+    color_code[53][0] = 153; color_code[53][1] = 0; color_code[53][2] = 255;
+
+    // Initialize CAPE
+    // plane_detector = new CAPE(height, width, PATCH_SIZE, PATCH_SIZE, cylinder_detection, COS_ANGLE_MAX, MAX_MERGE_DIST);
+    param = new CapeParam(PATCH_SIZE);
+
+
+    // init ROS
+    ros::init(argc, argv, "cape");
+    ros::NodeHandle n;
+    image_transport::ImageTransport it(n);
+
+    image_transport::SubscriberFilter sub_colour(it, "colour", 1);
+    image_transport::SubscriberFilter sub_depth(it, "depth", 1);
+    //tf::TransformListener listener; //tf2_msgs::TFMessage
+
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> ApproximateTimePolicy;
+    message_filters::Synchronizer<ApproximateTimePolicy> sync(ApproximateTimePolicy(5), sub_colour, sub_depth);
+
+    sync.registerCallback(boost::bind(&callback, _1, _2, param));
+
+    while(1) {
+        ros::spinOnce();
+    }
+
+    return EXIT_SUCCESS;
+
+}
+
